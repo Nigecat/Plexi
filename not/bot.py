797ce59d29664c312feaefc2febe7101c44bbddf
Nigecat <nigel.quick@carey.com.au>
2019-11-16 16:02:07 +1100

    added a selfbot

--- not/bot.py
+++ not/bot.py
@@ -24,26 +24,27 @@ async def on_ready():
 async def on_message(message):
     channel = bot.get_channel(message.channel.id)
     response = getResponse(message.content)
-    print(response)
-    if response != None and message.author.id != 638280239625273364 and message.channel.id == 638659193913737226:
+    if response != None and message.author.id != 638280239625273364 and (message.channel.id == 638659193913737226 or message.guild.id == 621181741972979722):
         await channel.send(response)
     await bot.process_commands(message)
 
 @bot.command()
-async def getSpeech398247293847923648173648(ctx, user: discord.Member):
+async def get(ctx, user: discord.Member):
     await ctx.message.delete()
 
-    messages = await ctx.channel.history(limit = 1000).flatten()
+    messages = await ctx.channel.history(limit = 3000).flatten()
     total = []
     for message in range(len(messages)):
-        if messages[message].author.id == user.id and messages[message - 1].author.id != user.id:
+        if messages[message].author.id == user.id and messages[message - 1].author.id != user.id and messages[message].author.id != messages[message - 1].author.id:
             total.append(messages[message - 1].content)
             total.append(messages[message].content)
+
+    total.reverse()
 
     for i in range(len(total)):
         total[i] = replace_trash(total[i])
 
-    with open("data.txt", "w") as f:
+    with open("data.txt", "a+") as f:
         f.write("\n".join(total))
 
 bot.run(TOKEN)
