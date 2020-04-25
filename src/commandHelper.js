c94c3b831a118ca3afbcccc148c5472bb14a0165
Nigecat <nigel.quick@carey.com.au>
2020-04-25 20:12:47 +1000

    Add comments

--- src/commandHelper.js
+++ src/commandHelper.js
@@ -2,10 +2,11 @@ const { readdir, readdirSync } = require("fs");
 const { MessageEmbed } = require("discord.js");
 
 module.exports = async function(message, database, client) {
-    database.getServerInfo(message.guild.id, row => {
+    database.getServerInfo(message.guild.id, row => {       // get prefix for server
         const prefix = row.prefix;
 
         if (message.content == "$help" || message.content == `${prefix}help`) {
+            // create custom message embed for help command
             const embed = new MessageEmbed()
                 .setColor([114, 137, 218])
                 .setAuthor(client.user.username, client.user.avatarURL())
@@ -13,9 +14,10 @@ module.exports = async function(message, database, client) {
                 .setFooter("v" + require("./package.json").version)
                 .setTitle(`This server's prefix is currently: ${prefix}`)
 
+            // loop through each command and append the required information to the embed
             readdirSync("./commands/public").forEach(file => {
                 let data = require(`./commands/public/${file}`);
-                if (data.perms.length > 0) {
+                if (data.perms.length > 0) {    // check if command has any perms and change what is displayed
                     embed.addField(`${prefix}${file.split(".")[0]} ${data.args.join(" ")}`, `${data.description} | Perms: ${data.perms.join(" ")}`);
                 } else {
                     embed.addField(`${prefix}${file.split(".")[0]} ${data.args.join(" ")}`, data.description);
@@ -28,12 +30,13 @@ module.exports = async function(message, database, client) {
             let command = message.content.split(prefix)[1].replace(/ .*/,'').toLowerCase();   // extract command
             let args = message.content.split(" ").slice(1);         // extract args
 
+            // looop through each command
             readdir("./commands/public", (err, files) => {
                 files.forEach(file => {
-                    if (file.split(".")[0] == command) {
-                        let data = require(`./commands/public/${file}`);
-                        if (message.member.hasPermission(data.perms)) {
-                            if (data.args.length == args.length) {
+                    if (file.split(".")[0] == command) { // check if file matches the user's command
+                        let data = require(`./commands/public/${file}`);    
+                        if (message.member.hasPermission(data.perms)) { // verify the user has the correct permissions
+                            if (data.args.length == args.length) {    // verify the user has entered all the arguments
                                 data.call(message, args);
                             } else {
                                 message.channel.send(`Command syntax error, expected syntax: \`${prefix}${command} ${data.args.join(" ")}\``)
