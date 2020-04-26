0675a40918654b1ed192228108dc998037794e8b
Nigecat <nigel.quick@carey.com.au>
2020-04-26 11:43:31 +1000

    Update description

--- src/commands/public/kick.js
+++ src/commands/public/kick.js
@@ -9,7 +9,7 @@ module.exports = {
                 message.channel.send(`**Successfully kicked:** ${message.mentions.members.first().user.tag}`);
             }).catch(err => {
                 message.channel.send("It appears something has gone wrong, chances are you attempted to kick someone who has a higher role then this bot.");
-            });;
+            });
         } catch (err) {
             message.channel.send("It appears something has gone wrong, chances are you didn't @ a valid user");
         }
