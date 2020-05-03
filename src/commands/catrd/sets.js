acef1f52115bb24420c0643460e2c3ffb0c67243
Nigecat <nigel.quick@carey.com.au>
2020-05-03 15:31:36 +1000

    Fix description

--- src/commands/catrd/sets.js
+++ src/commands/catrd/sets.js
@@ -4,7 +4,7 @@ const Config = require("../../data/config.json");
 
 module.exports = {
     args: [],
-    description: "View all valid sets and their costs",
+    description: "View all valid sets and their costs, run catrd setinfo <set> to view the cards in a set",
     call: function(message, args) {
         let database = new Database(Config.database, Config.default_prefix);
         let embed = new MessageEmbed()
