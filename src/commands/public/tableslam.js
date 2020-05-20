648c0d28cd2b8072745f632f635f7bc82ca2d525
Nigecat <nigel.quick@carey.com.au>
2020-05-21 08:14:59 +1000

    Clear files

--- src/commands/public/tableslam.js
+++
@@ -1,9 +0,0 @@
-
-module.exports = {
-    args: [],
-    perms: [],
-    description: "Slam someone against a table",
-    call: function(message) {
-        message.channel.send({files: ["./commands/resources/tableslam.png"]})
-    }
-}
