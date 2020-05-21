c952b1193f48c840fe7bd344590a724e532b1bf2
Nigecat <nigel.quick@carey.com.au>
2020-05-21 15:16:45 +1000

    Properly update the command helper

--- src/commandHelper.ts
+++ src/commandHelper.ts
@@ -1,13 +1,86 @@
 import { logBlue } from "./util/colour.js";
 import Server from "./util/Server.js";
-import { Message, Client } from "discord.js";
+import { Message, Client, MessageEmbed } from "discord.js";
 import Database from "./util/Database.js";
-import { existsSync } from "fs";
+import { existsSync, promises as fs } from "fs";
 
 export default async function processCommand(message: Message, database: Database, client: Client): Promise<void> {
-    let server = new Server(message.guild.id, database);
+    const server: Server = new Server(message.guild.id, database);
 
-    if (message.content.startsWith(server.prefix)) {
+
+    // Custom code for 264163117078937601 (Pinpointpotato#9418) AKA the ideas man
+    if (message.author.id == "264163117078937601" && message.content.toLowerCase().includes("you know why they call me the ideas man")) {
+        message.channel.send("cuz i CLEEEEEEAaaan up");
+    } 
+
+    
+    // Literally just pseudo-nitro (since bots can use emotes like how nitro does)
+    else if (message.content.startsWith("g`")) {
+        // Remove first two characters of message (g`)
+        message.content = message.content.slice(2);     
+
+        // Find all matching emojis and get the id and whether or not it is animated
+        const emojis: (string|boolean)[][] = client.emojis.cache.filter(emoji => emoji.name == message.content).map(emoji => [ emoji.id, emoji.animated ]);
+
+        // If an emoji was found
+        if (emojis.length > 0) {
+            const embed: MessageEmbed = new MessageEmbed()
+                .setImage(`https://cdn.discordapp.com/emojis/${emojis[0][0]}${emojis[0][1] ? ".gif" : ".png"}`)
+                .setFooter(message.author.tag);
+            message.channel.send({embed});
+            message.delete();
+        }
+    }
+
+
+    // General help command that lists the commands
+    else if (message.content == "$help" || message.content == `${server.prefix}help`) {
+        const embed: MessageEmbed = new MessageEmbed()
+            .setColor("#7289DA ")
+            .setTitle(`This server's prefix is currently: ${server.prefix}`)
+            .setAuthor(client.user.username, client.user.avatarURL())
+            .setTimestamp(new Date())
+            .setFooter(`v${(await import("./package.json")).default.version}`);
+
+        // Get a list of valid commands based on the files in /commands/public/
+        let commands: string[] = (await fs.readdir("./commands/public/"))   // Read files
+            .filter(file => file.endsWith(".js"))                       // Remove all non .js files
+            .map(file => `${server.prefix}${file.split(".")[0]}`);   // Set command text to {prefix}{commandName}
+
+        embed.addField(`(use ${server.prefix}help <command> to get more details on a command)`, commands.join("\n"));
+
+        message.channel.send({embed});
+    }
+
+
+    // Help command to get information on a specific command
+    else if (message.content.startsWith(`${server.prefix}help`)) {
+        const file: string = `./commands/public/${message.content.split(" ")[1]}.js`;
+
+        // If the command exists
+        if (existsSync(file)) {
+            let data: any = (await import(file)).default;
+
+            // If no perms default to displaying NONE
+            if (!data.perms) {
+                data.perms = ["NONE"];
+            }
+
+            // If args are string wrap them in []
+            if (typeof data.args == "string") {
+                data.args = `[${data.args}]`;
+
+            // Otherwise wrap each argument in <>
+            } else {
+                data.args = data.args.map((arg: string) => `<${arg}>`);
+            }
+            message.channel.send(`\`\`\`markdown\n# Command\n${server.prefix}${file.split(".")[0]} ${data.args}\n\n# Description\n${data.description}\n\n# Required Permissions\n${data.perms.join(", ")}\`\`\``);
+        }
+    }
+
+
+    // General command runner
+    else if (message.content.startsWith(server.prefix)) {
         logBlue(`[status] Executing command  [${message.content}]  from ${message.author.tag} in ${message.guild.name}`);
 
         // Extract first word after the prefix
@@ -21,7 +94,7 @@ export default async function processCommand(message: Message, database: Databas
 
         // If the command is found
         if (existsSync(file)) {
-            const data = (await import(file)).default;
+            const data: any = (await import(file)).default;
 
             // Ensure the user has the permissions to run this command
             if (!data.perms && !message.member.hasPermission(data.perms)) {
