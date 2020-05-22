6f425198846d026a5a0a11ce18d0fc30941c9a20
Nigecat <48661288+Nigecat@users.noreply.github.com>
2020-05-22 15:38:50 +1000

    Add new commands (#8)
    * Add image manipulation commands
    
    * Fix missing function docs
    
    * Add displayhex.ts
    
    * Add define and defineud commands
    
    * Make bot set to typing during long running command (Fix issue #6)
    
    * Add worm.ts
    
    * Add roll.ts
    
    * Add shout.ts

---
+++ src/commands/public/worm.ts
@@ -0,0 +1,16 @@
+import { Message } from "discord.js";
+
+export default {
+    args: ["length"],
+    description: "Create a worm of the specified length",
+    call (message: Message, args: string[]): void {
+        const length: number = Number(args[0]);
+        if (!isNaN(length) && length >= 0) {
+            message.channel.send(`<:h_:708133267366477944>${"<:b_:708133266644926505>".repeat(length)}<:t_:708133266657640578>`).catch(() => {
+                message.channel.send("That worm would be too long to post!");
+            });
+        } else {
+            message.channel.send("Length must be a positive number!");
+        }
+    }
+}
