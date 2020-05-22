257e56d714cc3e552c4395bb5e15ba00a93e4e1d
Nigecat <48661288+Nigecat@users.noreply.github.com>
2020-05-22 14:12:01 +1000

    Add new commands (#4)
    Add commands: ban, burn, mock
    Add Jimp module for image manipulation
    Cleanup util file and add function docs

---
+++ src/commands/public/ban.ts
@@ -0,0 +1,19 @@
+import { Message, GuildMember } from "discord.js";
+
+export default {
+    args: ["@user"],
+    perms: ["BAN_MEMBERS"],
+    description: "Ban a user",
+    call (message: Message): void {
+        const member: GuildMember = message.mentions.members.first();
+        try {
+            member.ban().then(() => {
+                message.channel.send(`**Successfully banned:** ${member.user.tag}`);
+            }).catch(err => {
+                message.channel.send("It appears something has gone wrong, chances are you attempted to ban someone who has a higher role than this bot.");
+            });
+        } catch (err) {
+            message.channel.send("It appears something has gone wrong, chances are you didn't @ a valid user");
+        }
+    }
+}
--- src/util/util.ts
+++ src/util/util.ts
@@ -1,5 +1,14 @@
-import { Message, Collection, Snowflake, TextChannel, DMChannel, NewsChannel } from "discord.js";
+import { Message, Collection, Snowflake, TextChannel, DMChannel, NewsChannel, MessageAttachment } from "discord.js";
 
+/**
+ * Get the second last message in a given channel
+ * 
+ * @remarks
+ * Useful for finding the message before a command
+ * 
+ * @param channel The channel to look in
+ * @returns A promise containing the message that was found
+ */
 export async function lastMessage(channel: (TextChannel | DMChannel | NewsChannel)): Promise<Message> {
     return new Promise(async resolve => {
         const messages: Collection<Snowflake, Message> = await channel.messages.fetch({ limit: 2 });
@@ -7,6 +16,26 @@ export async function lastMessage(channel: (TextChannel | DMChannel | NewsChanne
     });
 }
 
-export function sendMarkdown(channel: (TextChannel | DMChannel | NewsChannel), data: string[]): void {
-    channel.send(`\`\`\`markdown\n${data.join("\n")}\`\`\``);
+
+/**
+ * Correctly format text for a discord markdown block
+ * 
+ * @param data An array, each element is a line of the markdown
+ * @returns The properly formated data ready to send directly to discord
+ */
+export function formatMarkdown(data: string[]): string {
+    return `\`\`\`markdown\n${data.join("\n")}\`\`\``;
+}
+
+
+/**
+ * Check if a message attachment is a valid image
+ * 
+ * @param msgAttach The message attachment object
+ * @returns Whether or not it is a valid image
+ */
+export function attachIsImage(msgAttach: MessageAttachment): boolean {
+    const url: string = msgAttach.url.toLowerCase();
+    // true if this url is a png or jpg image.
+    return url.endsWith("png") || url.endsWith("jpg");
 }
