d2c1942f4443b352af0ac52ae309387ac5c89c59
Nigecat <nigel.quick@carey.com.au>
2020-05-22 15:17:24 +1000

    Add roll.ts

---
+++ src/commands/public/roll.ts
@@ -0,0 +1,11 @@
+import { Message } from "discord.js";
+
+export default {
+    args: ["min", "max"],
+    description: "Get a random number between <min> and <max> (inclusive)",
+    call (message: Message, args: string[]) {
+        const min: number = Math.ceil(Number(args[0]));
+        const max: number = Math.floor(Number(args[1]));
+        message.channel.send(Math.floor(Math.random() * (max - min + 1)) + min);
+    }
+}
