35f8e3ed77a6cdca349f02d1faa65d15c9095299
Nigecat <48661288+Nigecat@users.noreply.github.com>
2020-05-22 20:29:03 +1000

    Add autorole system (#10)

---
+++ src/commands/public/autorole.ts
@@ -0,0 +1,23 @@
+import { Message } from "discord.js";
+import Database from "../../util/Database.js";
+import Server from "../../util/Server.js";
+
+export default {
+    args: ["set|clear", "@role"],
+    perms: ["ADMINISTRATOR"],
+    description: "Assign a role to be automatically added to a user when they join this server",
+    async call (message: Message, args: string[], database: Database): Promise<void> {
+        const id: string = args[0] === "set" 
+            ? message.mentions.roles.first().id
+            : args[0] === "clear"
+                ? ""
+                : null;
+
+        if (id !== null) {
+            const server: Server = new Server(message.guild.id, database);
+            await server.init();
+            message.channel.send("Autorole updated!");
+            server.update("autorole", id);
+        } 
+    }
+}
