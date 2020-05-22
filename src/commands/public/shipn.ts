11e6281011d0075cf2c3bcb62208a20f2b1ed6d0
Nigecat <nigel.quick@carey.com.au>
2020-05-22 18:45:02 +1000

    Add shipn.ts

---
+++ src/commands/public/shipn.ts
@@ -0,0 +1,15 @@
+import { Message } from "discord.js";
+
+export default {
+    args: ["@user1", "@user2"],
+    description: "Ship two users (merge their nicknames)",
+    call (message: Message) {
+        const user1: string = message.guild.member(message.mentions.members.first().user).displayName;
+        const user2: string = message.guild.member(message.mentions.members.last().user).displayName;
+        const firsthalf: string = user1.slice(0, Math.ceil(user1.length / 2));
+        const secondhalf: string = user2.slice(Math.ceil(user2.length / 2) * -1);
+
+        // Combine the first half of user1's nickname with the second half of user2's nickname
+        message.channel.send(`**${user1} 💞 ${user2} = ${firsthalf}${secondhalf}**`);
+    }
+}
