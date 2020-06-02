0674b5051cc674ce1d7b8add101dcd7a9edaa98d
Nigecat <nigel.quick@carey.com.au>
2020-06-02 15:44:12 +1000

    Improve user error messages

--- src/commands/public/play.ts
+++ src/commands/public/play.ts
@@ -2,22 +2,19 @@ import { Message } from "discord.js";
 import ytdl from "ytdl-core";
 import Command from "../../util/Command.js";
 
-function play(message: Message, url: string) {
-    message.member.voice.channel.join().then(connection => {
-        const dispatcher = connection.play(ytdl(url, { filter: "audioonly" }));
-        dispatcher.on("finish", () => message.guild.me.voice.channel.leave());
-    });
-}
-
 export default Command.create({
-    args: "url",
+    args: ["url"],
     description: "Play a youtube video into a vc",
     call (message: Message, args: string): void {
         if (message.member.voice.channel) {
-            if (args.toLowerCase().startsWith("https://youtube.com/") || args.toLowerCase().startsWith("https://youtu.be/")) {
-                play(message, args);
+            if (args.toLowerCase().startsWith("https://youtube.com/") || args.toLowerCase().startsWith("https://youtu.be/") || args.toLowerCase().startsWith("https://www.youtube.com/") || args.toLowerCase().startsWith("https://www.youtu.be/")) {
+                message.channel.send(`🎵  Playing audio from ${args}  🎵`);
+                message.member.voice.channel.join().then(connection => {
+                    const dispatcher = connection.play(ytdl(args, { filter: "audioonly" }));
+                    dispatcher.on("finish", () => message.guild.me.voice.channel.leave());
+                });
             } else {
-                message.channel.send("This must be a url, search terms aren't supporterd yet.");
+                message.channel.send("This must be a youtube url, search terms aren't supported yet.");
             }
         } else {
             message.channel.send("You must be in a vc to run this!");
