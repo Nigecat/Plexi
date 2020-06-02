c935ac1afb3de9c369d92f078ed1ad65dc3a8ef6
Nigecat <nigel.quick@carey.com.au>
2020-06-02 15:15:32 +1000

    Audio commands are back!

---
+++ src/commands/public/play.ts
@@ -0,0 +1,28 @@
+import { Message } from "discord.js";
+import ytdl from "ytdl-core";
+import Command from "../../util/Command.js";
+import auth from "../../data/auth.json";
+import youtubeSearch from "youtube-search";
+
+function play(message: Message, url: string) {
+    message.member.voice.channel.join().then(connection => {
+        const dispatcher = connection.play(ytdl(url, { filter: "audioonly" }));
+        dispatcher.on("finish", () => message.guild.me.voice.channel.leave());
+    });
+}
+
+export default Command.create({
+    args: "url",
+    description: "Play a youtube video into a vc",
+    call (message: Message, args: string): void {
+        if (message.member.voice.channel) {
+            if (args.toLowerCase().startsWith("https://youtube.com/") || args.toLowerCase().startsWith("https://youtu.be/")) {
+                play(message, args);
+            } else {
+                message.channel.send("This must be a url! Search terms aren't supporterd yet.");
+            }
+        } else {
+            message.channel.send("You must be in a vc to run this!");
+        }
+    }
+});
