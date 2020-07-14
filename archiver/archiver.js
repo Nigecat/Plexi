db51e2107e6e7bf28d8f0e96d02df5ffc5b1d8a1
Nigecat <nigel.quick@carey.com.au>
2020-07-14 18:56:44 +1000

    Fix webhook message sending

--- archiver/archiver.js
+++ archiver/archiver.js
@@ -14,20 +14,27 @@ function formatSnowflake(snowflake) {
     return `${date.toLocaleDateString()} - ${date.toLocaleTimeString()}`;
 }
 
+function formatMessage(message) {
+    if (message.type === "GUILD_MEMBER_JOIN") return { content: `${message.author} joined the server!`, reactions: message.reactions.cache };
+    else if (message.embeds.length > 0) return { id: message.id, author: message.author, content: message.content, reactions: message.reactions.cache, payload: { embed: message.embeds[0] } };
+    else if (message.attachments.size > 0) return { id: message.id, author: message.author, content: message.content, reactions: message.reactions.cache, payload: { files: Array.from(message.attachments.values()).map(attachment => attachment.attachment) } };
+    else return { id: message.id, author: message.author, content: message.content, reactions: message.reactions.cache, payload: {} };
+}
+
 /* Returns an array of all the messages in a given channel, the zeroth element is the first message in that channel */
 async function getMessages(channel) {
     let messages = [];
 
     // Get most recent message in channel
     const firstMessage = (await channel.messages.fetch({ limit: 2 })).first();
-    messages.push(firstMessage);
+    messages.push(formatMessage(firstMessage));
     let pos = firstMessage.id;
 
     while (true) {
         const found = await channel.messages.fetch({ limit: 50, before: pos });
         if (found.array().length === 0) break;
         pos = found.last().id;
-        messages = messages.concat(found.map(message => message));
+        messages = messages.concat(found.map(formatMessage));
         console.log(`Performing message jump [${formatSnowflake(found.last().id)}] | ${messages.length} messages found`);
     }
 
@@ -37,6 +44,13 @@ async function getMessages(channel) {
 
 /** Send an array of messages to a channel */
 async function sendMessages(channel, messages, startFrom = 0) {
+    console.log("Clearing webhooks...");
+    // Before we start clear all webhooks in the destination channel
+    const hooks = await channel.fetchWebhooks();
+    for (const hook of hooks.array()) {
+        await hook.delete("Clearing space");
+    }
+    
     let users = {};
 
     const progress = new cliProgress.SingleBar({}, cliProgress.Presets.shades_classic);
@@ -46,6 +60,13 @@ async function sendMessages(channel, messages, startFrom = 0) {
 
         // If we don't have a webhook for this user
         if (!(messages[i].author.id in users)) {
+            // If we have 10 webhooks which is the max then remove all of them before making this new one
+            if (Object.keys(users).length >= 10) {
+                for (let user in users) {
+                    await users[user].delete("Clearing space");
+                }
+            }
+
             // Make one
             users[messages[i].author.id] = await channel.createWebhook(messages[i].author.username, {
                 avatar: messages[i].author.displayAvatarURL(),
@@ -54,7 +75,18 @@ async function sendMessages(channel, messages, startFrom = 0) {
         }
 
         // Send the message content using the webhook
-        await users[messages[i].author.id].send(`[${messages[i].author} | ${formatSnowflake(messages[i].id)}]\n${messages[i].content}`);
+        //  If the previous message was by the same person don't include the extra author details
+        if (messages[i].author.id == messages[i > 0 ? i - 1 : i].author.id) {
+            var sent = await users[messages[i].author.id].send("no");
+        } else {
+            messages[i].content = `[${messages[i].author} | ${formatSnowflake(messages[i].id)}]\n${messages[i].content}`
+            var sent = await users[messages[i].author.id].send(Object.assign({}, messages[i].payload, { content: messages[i].content, allowedMentions: { users : [] } }));
+        }
+
+        // Add the reactions
+        messages[i].reactions.array().forEach(async emoji => {
+            await sent.react(emoji._emoji);
+        });
 
         // Sleep for 500ms between each message so we don't get rate limited
         await sleep(500);
@@ -64,7 +96,7 @@ async function sendMessages(channel, messages, startFrom = 0) {
 
     // Remove the webhooks
     for (let user in users) {
-        users[user].delete();
+        await users[user].delete("Finished archiving");
     }
 }
 
