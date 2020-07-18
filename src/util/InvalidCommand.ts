4731143eb66e74eddf57afdd31c2299b40704b7e
Nigecat <nigel.quick@carey.com.au>
2020-07-18 17:44:41 +1000

    Delete old code

--- src/util/InvalidCommand.ts
+++
@@ -1,8 +0,0 @@
-
-export default class InvalidCommand extends Error {
-    public name: string;
-    constructor(message: string) {
-        super(message);
-        this.name = "InvalidCommand";
-    }
-}
