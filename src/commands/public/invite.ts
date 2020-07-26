468caed16100237ca948569d3a608be84ffbcc2b
Nigecat <nigel.quick@carey.com.au>
2020-07-27 09:43:49 +1000

    Clear old files

--- src/commands/public/invite.ts
+++
@@ -1,9 +0,0 @@
-import { Message } from "discord.js";
-import Command from "../../util/Command.js";
-
-export default Command.create({
-    description: "Get a bot invite link",
-    call (message: Message): void {
-        message.channel.send("https://discordapp.com/oauth2/authorize?client_id=621179289491996683&permissions=8&scope=bot");
-    }
-});
