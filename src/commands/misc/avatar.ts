1ef71e8e9a2e288d4bb865f11435a781b113b79f
Nigecat <48661288+Nigecat@users.noreply.github.com>
2020-07-28 08:53:54 +1000

    Add avatar command

---
+++ src/commands/misc/avatar.ts
@@ -0,0 +1,29 @@
+import { User, MessageEmbed } from "discord.js";
+import { Command, Client, CommandoMessage } from "discord.js-commando";
+
+export default class Avatar extends Command {
+    constructor(client: Client) {
+        super(client, {
+            name: "avatar",
+            memberName: "avatar",
+            description: "Get the mentioned user's avatar",
+            group: "misc",
+            args: [
+                {
+                    key: "user",
+                    prompt: "The user to get the avatar of.",
+                    type: "user"
+                }
+            ] 
+        });
+    }
+
+    run(message: CommandoMessage, { user }: { user: User }) {
+        const embed = new MessageEmbed({
+            color: "#0099ff",
+            title: user.tag,
+            image: { url: user.avatarURL({ dynamic: true, format: "png", size: 512 }) }
+        });
+        return message.embed(embed);
+    }
+}
