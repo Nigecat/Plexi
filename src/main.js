26cb0b3818a245048ce6b697926cf84638de2258
Nigecat <nigel.quick@carey.com.au>
2020-07-27 17:51:47 +1000

    Add client connector

--- src/main.js
+++ src/main.js
@@ -7,8 +7,16 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
         step((generator = generator.apply(thisArg, _arguments || [])).next());
     });
 };
-import * as token from "./config/auth.json";
+import { CommandClient } from "eris";
+import auth from "./config/auth.json";
+import config from "./config/config.json";
+import package_json from "../package.json";
 (() => __awaiter(void 0, void 0, void 0, function* () {
-    console.log(token.default.token);
+    const client = new CommandClient(auth.token, {}, { prefix: config.prefix, description: package_json.description, owner: `<@${config.owner}>` });
+    client.on("ready", () => {
+        console.log(`Logged in as ${client.user.username}#${client.user.discriminator}`);
+        client.editStatus("online", { name: "Visual Studio Code", type: 0 });
+    });
+    client.connect();
 }))();
 //# sourceMappingURL=main.js.map
--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,16 @@
 import { CommandClient } from "eris";
-import * as token from "./config/auth.json";
-//import { description } from "../package.json";
-//import { prefix, owner } from "./config/config.json";
+import auth from "./config/auth.json";
+import config from "./config/config.json";
+import package_json from "../package.json";
 
 // Wrap our code in an async functinon so we can use await
 (async() => {
+    const client = new CommandClient(auth.token, {}, { prefix: config.prefix, description: package_json.description, owner: `<@${config.owner}>` })
 
-    //const client = new CommandClient(token, {}, { prefix, description, owner: `<@${owner}>` })
+    client.on("ready", () => {
+        console.log(`Logged in as ${client.user.username}#${client.user.discriminator}`);
+        client.editStatus("online", { name: "Visual Studio Code", type: 0 });
+    });
+
+    client.connect();
 })();
