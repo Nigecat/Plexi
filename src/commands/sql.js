2d82351222fe7dbd04c090cbfb91d3e1ccc41507
Nigecat <nigel.quick@carey.com.au>
2020-07-27 16:02:18 +1000

    Fix bot not typing while executing long command

--- src/commands/sql.js
+++ src/commands/sql.js
@@ -3,9 +3,9 @@ const { owner } = require("../config/config.json");
 module.exports = {
     options: {
         description: "Execute an sql query in the database",
-        requirements: { roleIDs: [owner] }
+        requirements: { userIDs: [owner] }
     },
-    call({ message, args, database, client }) {
-        console.log(database);
+    async call({ message, args, database, client }) {
+
     }        
 }
--- src/main.js
+++ src/main.js
@@ -17,14 +17,19 @@ async function main() {
         // Remove the file extension from the file, this is the name of it and the command used to call it
         const commandName = command.split(".").slice(0, -1).join(" ");
         // Add our extra arguments 
-        client.registerCommand(commandName, (message, args) => data.call({ message, args, database, client }), data.options);
+        client.registerCommand(commandName, async (message, args) => {
+            message.channel.sendTyping();
+            return await data.call({ message, args, database, client });
+        }, data.options);
     });
+
     
     client.on("ready", () => {
         console.log(`Logged in as ${client.user.username}#${client.user.discriminator}`);
         client.editStatus("online", { name: "Visual Studio Code" });
     });
 
+    // Set all of our server prefixes
     (await database.getAllServers()).forEach(server => client.registerGuildPrefix(server.id, server.prefix));
     
     client.connect();
