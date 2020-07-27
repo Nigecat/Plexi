5a954a164c4375c9292cf31fbc3fa2084acfc7da
Nigecat <nigel.quick@carey.com.au>
2020-07-27 22:53:27 +1000

    Add database and setup sqlite provider

--- src/main.ts
+++ src/main.ts
@@ -1,26 +1,33 @@
+import { open } from "sqlite";
+import { Database } from "sqlite3";
 import { token } from "./config/auth.json";
 import * as config from "./config/config.json";
-import { CommandoClient } from "discord.js-commando";
 import { resolve as pathResolve, join as pathJoin } from "path";
+import { CommandoClient, SQLiteProvider } from "discord.js-commando";
 
-const client = new CommandoClient(config);
+(async() => {
+    const client = new CommandoClient(config);
 
-client.on("ready", () => {
-    console.log(`Logged in as ${client.user.tag}`);
-    client.user.setPresence({ status: "online", activity: { type: "PLAYING", name: config.commandPrefix + "help" } });
-});
+    const db = await open({ filename: pathJoin(pathJoin(pathJoin(pathResolve(), "src"), "data"), "data.sqlite"), driver: Database });
+    client.setProvider(new SQLiteProvider(db));
 
-client.on("debug", console.log);
-client.on("error", console.error);
-process.on("unhandledRejection", console.error);
-
-client.registry
-    .registerDefaultTypes()
-    .registerGroups([
-        ["misc", "Miscellaneous Commands"]
-    ])
-    .registerDefaultGroups()
-    .registerDefaultCommands()
-    .registerCommandsIn(pathJoin(pathJoin(pathResolve(), "src"), "commands"));
-
-client.login(token);
+    client.on("ready", () => {
+        console.log(`Logged in as ${client.user.tag}`);
+        client.user.setPresence({ status: "online", activity: { type: "PLAYING", name: config.commandPrefix + "help" } });
+    });
+    
+    client.on("debug", console.log);
+    client.on("error", console.error);
+    process.on("unhandledRejection", console.error);
+    
+    client.registry
+        .registerDefaultTypes()
+        .registerGroups([
+            ["misc", "Miscellaneous Commands"]
+        ])
+        .registerDefaultGroups()
+        .registerDefaultCommands()
+        .registerCommandsIn(pathJoin(pathJoin(pathResolve(), "src"), "commands"));
+    
+    client.login(token);
+})();
