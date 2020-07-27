1a45ee18c78bcac805c56583bf285aa34467ee4c
Nigecat <48661288+Nigecat@users.noreply.github.com>
2020-07-28 08:09:27 +1000

    Split config file

--- src/main.ts
+++ src/main.ts
@@ -2,18 +2,19 @@ import { open } from "sqlite";
 import { Database } from "sqlite3";
 import { token } from "./config/auth.json";
 import * as config from "./config/config.json";
+import * as clientConfig from "./config/clientConfig.json";
 import { resolve as pathResolve, join as pathJoin } from "path";
 import { CommandoClient, SQLiteProvider } from "discord.js-commando";
 
 (async() => {
-    const client = new CommandoClient(config);
+    const client = new CommandoClient(clientConfig);
 
-    const db = await open({ filename: pathJoin(pathJoin(pathJoin(pathResolve(), "src"), "data"), "data.sqlite"), driver: Database });
+    const db = await open({ filename: config.databasePath, driver: Database });
     client.setProvider(new SQLiteProvider(db));
 
     client.on("ready", () => {
         console.log(`Logged in as ${client.user.tag}`);
-        client.user.setPresence({ status: "online", activity: { type: "PLAYING", name: config.commandPrefix + "help" } });
+        client.user.setPresence({ status: "online", activity: { type: "PLAYING", name: clientConfig.commandPrefix + "help" } });
     });
     
     client.on("debug", console.log);
@@ -23,7 +24,7 @@ import { CommandoClient, SQLiteProvider } from "discord.js-commando";
     client.registry
         .registerDefaultTypes()
         .registerGroups([
-            ["misc", "Miscellaneous Commands"]
+            ["misc", "Miscellaneous"]
         ])
         .registerDefaultGroups()
         .registerDefaultCommands()
