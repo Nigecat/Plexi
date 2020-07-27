6a4355bf90f40833b31a4ea617fa8f53449d1c2f
Nigecat <nigel.quick@carey.com.au>
2020-07-27 22:08:49 +1000

    Setup commando

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,25 @@
+import path from "path";
 import auth from "./config/auth.json";
 import config from "./config/config.json";
-import commando from "discord.js-commando";
-const { CommandoClient } = commando;
+import Commando from "discord.js-commando";
 
-const client = new CommandoClient(config);
+const client = new Commando.CommandoClient(config);
 
 client.on("ready", () => {
     console.log(`Logged in as ${client.user.tag}`);
     client.user.setPresence({ status: "online", activity: { type: "PLAYING", name: "Visual Studio Code" } });
 });
+
+client.on("debug", console.log);
+client.on("error", console.error);
+
+client.registry
+    .registerDefaultTypes()
+    .registerGroups([
+        ["misc", "Miscellaneous Commands"]
+    ])
+    .registerDefaultGroups()
+    .registerDefaultCommands()
+    .registerCommandsIn(path.join(path.join(path.resolve(), "src"), "commands"));
 
 client.login(auth.token);
