680dce8af9844fa7bc190d77ccdf346ac27a2cf0
Nigecat <nigel.quick@carey.com.au>
2020-07-28 14:36:13 +1000

    Add google command and allow clap command to be used on previous message

--- src/commands/misc/google.ts
+++ src/commands/misc/google.ts
@@ -1,17 +1,17 @@
 import { lastMessage } from "../../util.js";
 import { Command, Client, CommandoMessage } from "discord.js-commando";
 
-export default class Clap extends Command {
+export default class Google extends Command {
     constructor(client: Client) {
         super(client, {
-            name: "clap",
-            memberName: "clap",
+            name: "google",
+            memberName: "google",
+            description: "LMGTFY a search query, if no text is supplied it will use the previous message",
             group: "misc",
-            description: "Put the 👏 emoji in the spaces of the specified text, if no text is supplied it will use the previous message",
             args: [
                 {
                     key: "text",
-                    prompt: "What text do you want to apply this to?",
+                    prompt: "What text do you want to LMGTFY?",
                     type: "string",
                     default: "USE_PREVIOUS"
                 }
@@ -22,7 +22,7 @@ export default class Clap extends Command {
     async run(message: CommandoMessage, { text }: { text: string }) {
         // Check if we are defaulting to the previous message as the target text
         if (text === "USE_PREVIOUS") text = (await lastMessage(message.channel)).content;
-
-        return message.say(`👏 ${text.replace(" ", " 👏 ")} 👏`);
+        
+        return message.say(encodeURI("http://lmgtfy.com/?q=" + text));
     }
 }
