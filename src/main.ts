451afad2ac8bde6c4f38f234f150c0ae0fab8850
Nigecat <nigel.quick@carey.com.au>
2020-07-28 16:30:32 +1000

    Move token to an environment variable This allows the typescript compiler to build the project without the original auth.json file existing, now we should be able to build it from a workflow

--- src/main.ts
+++ src/main.ts
@@ -1,20 +1,13 @@
 import { open } from "sqlite";
-import { existsSync } from "fs";
 import { Database } from "sqlite3";
+import { config as loadEnv } from "dotenv";
 import * as config from "./config/config.json";
 import * as clientConfig from "./config/clientConfig.json";
 import { resolve as pathResolve, join as pathJoin } from "path";
 import { CommandoClient, SQLiteProvider } from "discord.js-commando";
 
-// If the auth file has not been created
-if (!existsSync("src/config/auth.json")) {
-    console.error("No auth file found! Create a json file at `src/config/auth.json` and ensure it has a token field with the bot token in it.");
-    process.exit(1);
-}
-
 (async() => {
-    // Now we know the auth file has to exist so we can safetly import it
-    const { token } = await import("./config/auth.json");
+    loadEnv();
 
     const client = new CommandoClient(clientConfig);
 
@@ -40,5 +33,5 @@ if (!existsSync("src/config/auth.json")) {
         .registerDefaultCommands({ unknownCommand: false })
         .registerCommandsIn(pathJoin(pathJoin(pathResolve(), "src"), "commands"));
     
-    client.login(token);
+    client.login(process.env.TOKEN);
 })();
