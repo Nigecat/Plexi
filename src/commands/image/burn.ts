7d2d3f9ad0c5c401e2925cbaa7bb362069f67219
Nigecat <48661288+Nigecat@users.noreply.github.com>
2020-07-29 20:45:16 +1000

    Add support for running image manipulation commands with urls (#32)

--- src/commands/image/burn.ts
+++ src/commands/image/burn.ts
@@ -8,28 +8,35 @@ export default class Burn extends Command {
             name: "burn",
             memberName: "burn",
             group: "image",
-            description: "Burn an image, NOTE: This will act on the image in the previous message",
-            clientPermissions: ["ATTACH_FILES"],
+            description: "Burn an image, NOTE: If no url is supplied it will act on the previous message",
+            userPermissions: ["EMBED_LINKS", "ATTACH_FILES"],
+            clientPermissions: ["EMBED_LINKS", "ATTACH_FILES"],
             args: [
                 {
                     key: "url",
                     prompt: "What is the url of the image you want to burn?",
                     type: "string",
-                    validate: isURL,
                     default: "USE_PREVIOUS"
                 }
             ]
         });
     }
 
-    async run(message: CommandoMessage) {
+    async run(message: CommandoMessage, { url }: { url: string }) {
         let response: Promise<Message | Message[]>;
         message.channel.startTyping();
 
         try {
-            const url = (await lastMessage(message.channel)).attachments.first().url;
-            const result = await manipulateImage(url, 3);
-            response = message.say({ files: [ result ] });
+            // Check if we are defaulting to the previous message as the target text
+            if (url === "USE_PREVIOUS") url = (await lastMessage(message.channel)).attachments.first().url;
+
+            // If the url is a discord attachment
+            if (/(http|https):\/\/cdn.(discord|discordapp).com\/attachments\//.test(url)) {
+                const result = await manipulateImage(url, 3);
+                response = message.say({ files: [ result ] });
+            } else {
+                response = message.say("The image must be a discord attachment!");
+            }
         } catch {
             response = message.say("Unsupported file type (or the previous message was not an image)");
         }
