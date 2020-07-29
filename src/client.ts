2e178326daeea57e025850df117b058b429161a3
Nigecat <48661288+Nigecat@users.noreply.github.com>
2020-07-29 12:57:01 +1000

    Migrate data from previous version

--- src/client.ts
+++ src/client.ts
@@ -4,10 +4,14 @@ import { CommandoClient, CommandoClientOptions } from "discord.js-commando";
 export class PlexiClient extends CommandoClient {
     constructor(options?: CommandoClientOptions) {
         super(options);
-        this.data = {};
+        this.data = {
+            servers: {}
+        };
     }
 
     data: {
-        autoroles?: Keyv
+        servers: {
+            autoroles?: Keyv
+        }
     }
 }
