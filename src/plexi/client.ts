a377c9829398606d51e90a7c9894e8c63138370b
Nigecat <nigel.quick@carey.com.au>
2020-07-30 13:57:47 +1000

    Add custom prefix handler

--- src/plexi/client.ts
+++ src/plexi/client.ts
@@ -1,28 +1,35 @@
 import { promises as fs } from "fs";
 import { Command } from "./command";
+import { DataStore } from "./datastore";
 import { resolve, extname } from "path";
 import { Client, ClientOptions, Message } from "discord.js";
 
 export class PlexiClient extends Client {
-    /* eslint-disable @typescript-eslint/ban-types */
     public readonly defaultPrefix: string;
+    public readonly databasePath: string;
     public readonly commands: {(name: string): Command}[] | {};
     public readonly prefixCache: {(id: string): RegExp}[] | {};
-    /* eslint-enable @typescript-eslint/ban-types */
+    public readonly data: { prefixes: DataStore };
 
     constructor(options: PlexiOptions) {
         const defaultPrefix = options.defaultPrefix;
+        const databasePath = options.databasePath;
         delete options.defaultPrefix;
+        delete options.databasePath;
         super(options);
 
         this.defaultPrefix = defaultPrefix;
+        this.databasePath = databasePath;
         this.commands = {};
         this.prefixCache = {};
+        this.data = { prefixes: new DataStore(this.databasePath, "prefix") };
 
         this.on("message", this.onMessage);
     }
 
     async registerCommands(dir: string) {
+        await this.data.prefixes.connect();
+
         for (const group of await fs.readdir(dir)) {
             for (const file of await fs.readdir(resolve(dir, group))) {
                 // Ignore any non javascript files
@@ -41,11 +48,24 @@ export class PlexiClient extends Client {
         }
     }
 
-    onMessage(message: Message) {
-        console.log(message);
+    async onMessage(message: Message) {
+        // Get this server's prefix, or default to the set default prefix if it does not exist
+        const prefix = await this.data.prefixes.get(message.guild.id) || this.defaultPrefix;
+
+        // If we don't have the prefix regex cached then we generate it
+        if (!(prefix in this.prefixCache)) { 
+            this.prefixCache[prefix] = new RegExp(`^(<@!?${this.user.id}>\\s+(?:\\${prefix}\\s*)?|\\${prefix}\\s*)([^\\s]+)`, "i");
+            console.log(`Built command pattern for prefix "${prefix}": ${this.prefixCache[prefix]}`)
+        }
+
+        // If the message matches our prefix
+        if (this.prefixCache[prefix].test(message.content)) {
+            console.log(message.content);
+        }
     }
 }
 
 export interface PlexiOptions extends ClientOptions {
-    defaultPrefix: string
+    defaultPrefix: string,
+    databasePath: string
 }   
