aed5fbfc618a8c937548a10366bdbd87c84a767a
Nigecat <nigel.quick@carey.com.au>
2020-08-03 11:59:15 +1000

    Setup logger and add config types

--- index.ts
+++ index.ts
@@ -1,7 +1,44 @@
 import { config } from "dotenv";
 import { Plexi } from "./src/plexi";
+import { existsSync, mkdirSync } from "fs";
+import { createLogger, format, transports } from "winston";
 
+// Load any environment variables into process.env
 config();
+
+// If our log output dir does not exist create it
+if (!existsSync("logs")) {
+    mkdirSync("logs");
+}
+
+// Create our logger
+const logger = createLogger({
+    level: "info",
+    format: format.combine(format.timestamp(), format.json()),
+    transports: [
+        // Write all logs with level `error` and below to `error.log`
+        new transports.File({ filename: "logs/error.log", level: "error" }),
+        // Write all logs with level `info` and below to `combined.log`
+        new transports.File({ filename: "logs/combined.log" }),
+    ],
+});
+
+// If we're not in production then log to the console with the format:
+// `${info.level}: ${info.message} ${JSON.stringify({ ...rest })} `
+if (process.env.NODE_ENV !== "production") {
+    logger.add(
+        new transports.Console({
+            format: format.simple(),
+        }),
+    );
+}
+
 const client = new Plexi();
+
+client.on("ready", () => console.log(`Logged in as ${client.user.tag}`));
+client.on("debug", (data) => logger.info(data));
+client.on("error", (data) => logger.error(data));
+process.on("uncaughtException", (data) => logger.error(data));
+process.on("unhandledRejection", (data) => logger.error(data));
 
 client.login(process.env.DISCORD_TOKEN);
--- src/plexi.ts
+++ src/plexi.ts
@@ -5,15 +5,32 @@ import { Client, ClientOptions } from "discord.js";
  * @param {ClientOptions} options - The options for the client
  */
 export class Plexi extends Client {
-    constructor(options: PlexiOptions = {}) {
+    /** The options for this client */
+    public readonly config: PlexiOptions;
+
+    /** Create a new bot
+     * @param {Options} options - The options for this client
+     */
+    constructor(options: Options = {}) {
         super(options.client);
+        this.config = options.plexi;
     }
 }
 
-export interface PlexiOptions {
+/** The options for this client */
+export interface Options {
+    /** These options get passed onto the underlying discord.js [Client]{@link https://discord.js.org/#/docs/main/stable/class/Client}.
+     *  Must be a valid [ClientOptions]{@link https://discord.js.org/#/docs/main/stable/typedef/ClientOptions} object.
+     */
     client?: ClientOptions;
-    plexi?: {
-        supportServer?: string;
-        owner?: string;
-    };
+    /** These are the options specific to our extended client */
+    plexi?: PlexiOptions;
+}
+
+/** The options specific to our client */
+export interface PlexiOptions {
+    /** The id of the bot support server */
+    supportServer?: string;
+    /** The id of the bot owner */
+    owner?: string;
 }
