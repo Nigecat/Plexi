97e5f1aebac85f2bf8a82ad88a75c7d927f2c0b4
Nigecat <48661288+Nigecat@users.noreply.github.com>
2020-08-03 10:10:46 +1000

    Add docs and documentation generator

--- src/plexi.ts
+++ src/plexi.ts
@@ -1,3 +1,12 @@
-import { Client } from "discord.js";
+import winston from "winston";
+import { Client, ClientOptions } from "discord.js";
 
-export class Plexi extends Client {}
+/**
+ * An extended version of the discord.js client
+ * @param {ClientOptions} options - The options for the client
+ */
+export class Plexi extends Client {
+    constructor(options: ClientOptions) {
+        super(options);
+    }
+}
