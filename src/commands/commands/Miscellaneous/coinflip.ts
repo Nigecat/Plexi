21151594b1459d072b62d0a039b6566a9bb2910f
Nigecat <48661288+Nigecat@users.noreply.github.com>
2020-08-04 11:06:35 +1000

    Add coinflip command

---
+++ src/commands/commands/Miscellaneous/coinflip.ts
@@ -0,0 +1,17 @@
+import { Message } from "discord.js";
+import { Plexi } from "../../../Plexi";
+import { Command } from "../../Command";
+
+export default class CoinFlip extends Command {
+    constructor(client: Plexi) {
+        super(client, {
+            name: "coinflip",
+            group: "Miscellaneous",
+            description: "Flip a coin",
+        });
+    }
+
+    run(message: Message): void {
+        message.channel.send(Math.floor(Math.random() * 2) === 0 ? "Heads" : "Tails");
+    }
+}
