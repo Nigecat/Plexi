333f40332f7ffdec1a5b4f8d247fd799378c2186
Nigecat <nigel.quick@carey.com.au>
2020-08-05 14:11:27 +1000

    Remove momentjs

--- src/commands/commands/Util/getlogs.ts
+++ src/commands/commands/Util/getlogs.ts
@@ -1,5 +1,4 @@
 import ms from "ms";
-import moment from "moment";
 import { promises as fs } from "fs";
 import { Message } from "discord.js";
 import { Plexi } from "../../../Plexi";
@@ -34,21 +33,21 @@ export default class GetLogs extends Command {
             .filter((line) => line)
             .map((line) => JSON.parse(line));
 
-        const now = moment();
+        const expected = new Date(Date.now() - ms(timeframe));
 
-        const output = [];
-
-        data.forEach((line) => {
-            const then = moment(line.timestamp).add(ms(timeframe), "milliseconds");
-            if (then.isAfter(now) && (level === "all" || line.level === level) && !line.message.includes("token")) {
-                output.push(`${line.level}: ${line.message} (${line.timestamp})`);
-            }
-        });
+        const output = data
+            .filter(
+                (line) =>
+                    new Date(line.timestamp) >= expected &&
+                    (level === "all" || line.level === level) &&
+                    !line.message.includes("token"),
+            )
+            .map((line) => `${line.level}: ${line.message} (${line.timestamp})`);
 
         if (output.length > 0) {
             message.channel.send(output, { split: true });
         } else {
-            message.channel.send("No logs were found for that timeframe (and level)!");
+            message.channel.send("No logs were found for that timeframe (or level)");
         }
     }
 }
