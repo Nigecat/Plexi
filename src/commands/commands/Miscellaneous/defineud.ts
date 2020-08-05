021c4c21555ebb53c8612f66e8457285b92bf309
Nigecat <nigel.quick@carey.com.au>
2020-08-05 13:35:47 +1000

    Remove urban-dictionary

--- src/commands/commands/Miscellaneous/defineud.ts
+++ src/commands/commands/Miscellaneous/defineud.ts
@@ -1,7 +1,7 @@
-import { promisify } from "util";
+import querystring from "querystring";
 import { Plexi } from "../../../Plexi";
-import { term } from "urban-dictionary";
 import { Command } from "../../Command";
+import { fetch } from "../../../utils/misc";
 import { Message, MessageEmbed } from "discord.js";
 
 export default class DefineUD extends Command {
@@ -23,11 +23,10 @@ export default class DefineUD extends Command {
     async run(message: Message, [word]: [string]): Promise<void> {
         const result = await message.channel.send(`Searching for word: \`${word}\`...`);
 
-        // eslint-disable-next-line @typescript-eslint/no-explicit-any
-        let definitions: any;
+        let definitions: WordDefinition[];
 
         try {
-            definitions = await promisify(term)(word);
+            definitions = await this.getDefinitions(word);
         } catch {
             result.edit("Word not found!");
             return;
@@ -39,13 +38,36 @@ export default class DefineUD extends Command {
         });
 
         // Remove any definitions greater than the max embed field character limit
-        // eslint-disable-next-line @typescript-eslint/no-explicit-any
-        definitions = definitions.filter((definition: any) => definition.definition.length <= 1024);
+        definitions = definitions.filter((definition: WordDefinition) => definition.definition.length <= 1024);
 
         // Add all the definitions to the embed
-        // eslint-disable-next-line @typescript-eslint/no-explicit-any
-        definitions.forEach((definition: any, i: number) => embed.addField(i + 1, definition.definition));
+        definitions.forEach((definition: WordDefinition, i: number) => embed.addField(i + 1, definition.definition));
 
         result.edit("", { embed });
     }
+
+    async getDefinitions(term: string): Promise<WordDefinition[]> {
+        const query = querystring.stringify({ term });
+        const result = await fetch(`https://api.urbandictionary.com/v0/define?${query}`);
+
+        if (!result.list[0]) {
+            throw new Error(`${term} is undefined.`);
+        }
+
+        return result.list;
+    }
+}
+
+interface WordDefinition {
+    definition: string;
+    permalink: string;
+    thumbs_up: number;
+    thumbs_down: number;
+    sound_urls: string[];
+    author: string;
+    word: string;
+    defid: number;
+    current_vote: string;
+    written_on: string;
+    example: string;
 }
