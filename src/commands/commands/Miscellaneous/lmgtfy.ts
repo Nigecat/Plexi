947e046921674722c16e9755ee21cda741e9fc63
Nigecat <nigel.quick@carey.com.au>
2020-08-07 21:59:46 +1000

    Rename and fix google command (now lmgtfy)

--- src/commands/commands/Miscellaneous/lmgtfy.ts
+++ src/commands/commands/Miscellaneous/lmgtfy.ts
@@ -3,16 +3,17 @@ import { Command } from "../../Command";
 import { Message } from "discord.js";
 import { lastMessage } from "../../../utils/misc";
 
-export default class Google extends Command {
+export default class lmgtfy extends Command {
     constructor(client: Plexi) {
         super(client, {
-            name: "google",
+            name: "lmgtfy",
             group: "Miscellaneous",
             description: "LMGTFY a search query, if no text is supplied it will use the previous message",
             args: [
                 {
                     name: "text",
                     type: "string",
+                    infinite: true,
                     default: "USE_PREVIOUS",
                 },
             ],
@@ -23,6 +24,6 @@ export default class Google extends Command {
         // Check if we are defaulting to the previous message as the target text
         if (text === "USE_PREVIOUS") text = (await lastMessage(message.channel)).content;
 
-        message.channel.send(encodeURI(`http://lmgtfy.com/?q=${text}`));
+        message.channel.send(encodeURI(`http://lmgtfy.com/?q=${encodeURI(text)}`));
     }
 }
