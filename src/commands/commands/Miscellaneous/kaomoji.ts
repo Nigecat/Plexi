bae17d74722ec31efd08057a4a07aeaef1518d06
Nigecat <nigel.quick@carey.com.au>
2020-08-07 21:57:42 +1000

    Add kaomoji command

---
+++ src/commands/commands/Miscellaneous/kaomoji.ts
@@ -0,0 +1,20 @@
+import { Message } from "discord.js";
+import { Plexi } from "../../../Plexi";
+import { Command } from "../../Command";
+import kaomoji from "../../../assets/json/kaomoji.json";
+
+export default class Kaomoji extends Command {
+    constructor(client: Plexi) {
+        super(client, {
+            name: "kaomoji",
+            group: "Miscellaneous",
+            description:
+                "Display a random kaomoji! (´・ω・｀) 3000 will definitely be enough to keep you busy! (ｖ｀▽´)ｖ",
+        });
+    }
+
+    run(message: Message): void {
+        const face = kaomoji[Math.floor(Math.random() * kaomoji.length)];
+        message.channel.send(face);
+    }
+}
