c195b00496004a85366b8af163a5bf0909ba199c
Nigecat <48661288+Nigecat@users.noreply.github.com>
2020-08-08 19:46:38 +1000

    Add command alias support (#56)

--- src/events/voiceStateUpdate.ts
+++ src/events/voiceStateUpdate.ts
@@ -8,8 +8,11 @@ export default async function (client: Plexi, [state]: [VoiceState]): Promise<vo
         if (isAlone(client.user.id, state.guild.me.voice.channel)) {
             // Diconnect from the vc after 60 seconds if it is still just us
             setTimeout(() => {
-                if (isAlone(client.user.id, state.guild.me.voice.channel)) {
-                    state.guild.me.voice.channel.leave();
+                // If we are still in a voice channel (we may have been force disconnected)
+                if (state.guild.me.voice && state.guild.me.voice.channel) {
+                    if (isAlone(client.user.id, state.guild.me.voice.channel)) {
+                        state.guild.me.voice.channel.leave();
+                    }
                 }
             }, 60000);
         }
