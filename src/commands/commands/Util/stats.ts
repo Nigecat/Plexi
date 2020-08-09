bd0a7001dcd046661719cd7502f748ccd0580320
Nigecat <nigel.quick@carey.com.au>
2020-08-09 21:09:34 +1000

    Fix formatting in stats command

--- src/commands/commands/Util/stats.ts
+++ src/commands/commands/Util/stats.ts
@@ -17,10 +17,10 @@ export default class Stats extends Command {
             title: this.client.user.username + " Stats",
             thumbnail: { url: this.client.user.avatarURL({ dynamic: true, format: "png", size: 512 }) },
             fields: [
-                { name: "Total (Cached) Servers", value: this.client.guilds.cache.size, inline: true },
-                { name: "Total (Cached) Users", value: this.client.users.cache.size, inline: true },
-                { name: "Total (Cached) Channels", value: this.client.channels.cache.size, inline: true },
-                { name: "Memory Usage (MB)", value: process.memoryUsage().heapUsed / 1024 / 1024 },
+                { name: "Total Servers", value: this.client.guilds.cache.size, inline: true },
+                { name: "Total Users", value: this.client.users.cache.size, inline: true },
+                { name: "Total Channels", value: this.client.channels.cache.size, inline: true },
+                { name: "Memory Usage (MB)", value: process.memoryUsage().heapUsed / 1024 / 1024, inline: true },
                 { name: "Uptime (hours)", value: process.uptime() / 3600, inline: true },
                 {
                     name: "Useful Links",
