192403dcf01bbf70291c87cc9b1abf17a13ea3aa
Nigecat <nigel@nigelq.com>
2020-08-14 17:05:49 +1000

    feat(command): add cardinfo command

---
+++ src/commands/commands/Catrd/cardinfo.ts
@@ -0,0 +1,68 @@
+import { Plexi } from "../../../Plexi";
+import { Command } from "../../Command";
+import { Message, MessageEmbed, MessageAttachment } from "discord.js";
+
+export default class CardInfo extends Command {
+    constructor(client: Plexi) {
+        super(client, {
+            name: "cardinfo",
+            group: "Catrd",
+            description: "Get info on the specified card",
+            args: [
+                {
+                    name: "card",
+                    type: "string",
+                    infinite: true,
+                },
+            ],
+        });
+    }
+
+    run(message: Message, [cardSearch]: [string]): void {
+        if (this.client.cards.has(cardSearch)) {
+            const card = this.client.cards.get(cardSearch);
+            const image = new MessageAttachment(card.image, "card.jpg");
+            const embed = new MessageEmbed({
+                color: "RANDOM",
+                title: card.name,
+                files: [image],
+                image: { url: "attachment://card.jpg" },
+                fields: [
+                    {
+                        name: "Pack",
+                        value: card.pack,
+                        inline: true,
+                    },
+                    {
+                        name: "Type",
+                        value: card.type,
+                        inline: true,
+                    },
+                    {
+                        name: "Value",
+                        value: `${card.value} coins`,
+                        inline: true,
+                    },
+                    {
+                        name: "\u200b",
+                        value: "\u200b",
+                    },
+                    {
+                        name: "Power",
+                        value: card.power,
+                        inline: true,
+                    },
+                    {
+                        name: "Rarity",
+                        value: card.rarity,
+                        inline: true,
+                    },
+                ],
+            });
+
+            message.channel.send({ embed });
+        } else {
+            message.channel.send("I could not find that card");
+        }
+    }
+}
--- src/managers/CardManager.ts
+++ src/managers/CardManager.ts
@@ -3,7 +3,23 @@ import { Collection } from "discord.js";
 import basic from "../assets/cards/packs/basic.json";
 import mewtal_gear from "../assets/cards/packs/mewtal-gear.json";
 
-export default class CardManager extends Collection<string, Card> {
+/** Calculate the value (in coins) of a card */
+function calculateValue(rarity: number): number {
+    return Math.floor((30 - rarity) ** 1.433);
+}
+
+/** An extension of the discord.js collection that allows us to have case insensitive keys */
+class CaseInsensitiveCollection<T, U> extends Collection<T, U> {
+    set(key: T, value: U): this {
+        if (typeof key === "string") {
+            // eslint-disable-next-line @typescript-eslint/no-explicit-any
+            key = <T>(key.toLowerCase() as unknown);
+        }
+        return super.set(key, value);
+    }
+}
+
+export default class CardManager extends CaseInsensitiveCollection<string, Card> {
     constructor() {
         super();
 
@@ -13,6 +29,7 @@ export default class CardManager extends Collection<string, Card> {
                 pack: "Basic",
                 type: card.type as Card["type"],
                 power: card.power,
+                value: calculateValue(card.rarity),
                 rarity: card.rarity,
                 image: resolve(__dirname, "..", "assets", "cards", "images", "Basic", `${card.name}.jpg`),
             });
@@ -24,6 +41,7 @@ export default class CardManager extends Collection<string, Card> {
                 pack: "Mewtal Gear",
                 type: card.type as Card["type"],
                 power: card.power,
+                value: calculateValue(card.rarity),
                 rarity: card.rarity,
                 image: resolve(__dirname, "..", "assets", "cards", "images", "Mewtal Gear", `${card.name}.jpg`),
             });
@@ -45,4 +63,6 @@ export interface Card {
     rarity: number;
     /** A non-relative file path to this card's image */
     image: string;
+    /** The number of coins this is card is worth */
+    value: number;
 }
