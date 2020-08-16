9b994760c45d588c8aff28f28f49150c44d6e80f
Nigecat <nigel@nigelq.com>
2020-08-16 19:05:31 +1000

    Add custom file temp file generator

--- src/utils/image.ts
+++ src/utils/image.ts
@@ -1,3 +1,6 @@
+import { resolve } from "path";
+import { unlinkSync } from "fs";
+import { randomBytes } from "crypto";
 import { read as jimpRead } from "jimp";
 
 /**
@@ -17,7 +20,7 @@ export function isDiscordURL(url: string): boolean {
  * @param contrast The contrast level
  * @param pixelate The level of pixelation
  *
- * @returns The path of the output file
+ * @returns The path of the output file, it is up to the caller to delete this file
  */
 export async function manipulateImage(
     url: string,
@@ -29,7 +32,16 @@ export async function manipulateImage(
     const ext = "." + url.split(".").pop().split(/#|\?/)[0];
 
     // Create a temporary file with the matching extension so discord knows what to do with it when we upload the file
-    const { path } = "todo";
+    const path = resolve(
+        __dirname,
+        "..",
+        "assets",
+        "temp",
+        `image-${posterize}${contrast}${pixelate}-${randomBytes(6).readUIntLE(0, 6).toString(36)}.${ext}`,
+    );
+
+    // The file will automatically be deleted after one minute
+    setTimeout(() => unlinkSync(path), 60000);
 
     (await jimpRead(url)).pixelate(pixelate).posterize(posterize).contrast(contrast).write(path);
 
