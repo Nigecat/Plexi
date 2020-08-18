b4960308bff2ae0fd45aaaee4a084c3cfdb625e1
Nigecat <nigel@nigelq.com>
2020-08-18 22:11:30 +1000

    feat(command): help command for catrd

--- src/commands/commands/Catrd/catrd-help.ts
+++ src/commands/commands/Catrd/catrd-help.ts
@@ -1,7 +1,7 @@
 import { Plexi } from "../../../Plexi";
 import { Command } from "../../Command";
-import { Message, MessageEmbed } from "discord.js";
 import { stripIndents } from "common-tags";
+import { Message, MessageEmbed } from "discord.js";
 
 export default class CatrdHelp extends Command {
     constructor(client: Plexi) {
@@ -17,10 +17,32 @@ export default class CatrdHelp extends Command {
             color: "RANDOM",
             title: "Catrd: Its like Ryan Reynold’s Foolproof but better and a card game",
             description: stripIndents`
-                Get started by running the \`mycards\` command to view your cards.
-                Run \`bal\` to view how many coins you have. 
-                View the Economy section of the \`help\` command to see possible ways to get coins.
+                What is catrd?
+                It is a card game, but with cats! 
             `,
+            fields: [
+                {
+                    name: "Each card has the following:",
+                    value: stripIndents`
+                        **pack** - This is the general category of the card. You can buy 5 random cards from a specific pack with \`buypack\`.
+                        **type** (Melee | Defense | Scout) - This is the position the card gets played to on the board.
+                        **value** - The value in coins that this card is worth, if you have this card it can be sold with \`sellcard\`.
+                        **power** - The amount of power this card has in a duel (more is better!).
+                        **rarity** - The rarity of this card, this affects the chance to get it to drop when you buy a pack.
+                        **ability** - The ability of this card, run \`abilityinfo\` to check what an ability does. These only affect gameplay, not all cards will have one.
+                        
+                        Use the \`cardinfo\` command to get view this on a specific card (e.g \`cardinfo standard cat\`)
+                    `,
+                },
+                {
+                    name: "Getting started",
+                    value: stripIndents`
+                        Get sstarted by running the \`mycards\` command to view your cards.
+                        Run \`bal\` to view how many coins you have. 
+                        View the Economy section of the \`help\` command result to see possible ways to get coins (just try running some of the commands).
+                    `,
+                },
+            ],
         });
         message.channel.send({ embed });
     }
