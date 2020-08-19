775bc60b35f9a8a55f6d972c05707af5c4524d61
Nigecat <nigel@nigelq.com>
2020-08-19 13:59:19 +1000

    fix: extremely large decks not display properly

--- src/commands/commands/Catrd/mycards.ts
+++ src/commands/commands/Catrd/mycards.ts
@@ -1,6 +1,7 @@
 import { Plexi } from "../../../Plexi";
 import { Command } from "../../Command";
 import { Message, MessageEmbed } from "discord.js";
+import { ZERO_WIDTH_SPACE } from "../../../constants";
 
 export default class MyCards extends Command {
     constructor(client: Plexi) {
@@ -18,16 +19,19 @@ export default class MyCards extends Command {
         if (user.cards.length === 0 && user.deck.length === 0) {
             message.channel.send("You don't have any cards! Run `packinfo` to get started.");
         } else {
+            const cards = [{ name: "Cards", value: "", inline: true }];
+            user.cards.forEach((card) => {
+                if (cards[cards.length - 1].value.length >= 1024) {
+                    cards.push({ name: ZERO_WIDTH_SPACE, value: "", inline: true });
+                }
+                cards[cards.length - 1].value += card + "\n";
+            });
+
             const embed = new MessageEmbed({
                 color: "RANDOM",
                 title: "Your cards:",
                 footer: { text: `Run \`duel\` to fight another user with your deck (Deck: [${user.deck.length}/20])` },
-                fields: [
-                    {
-                        name: "Cards",
-                        value: user.cards.join("\n") || "You don't have any cards! Run `buypack` to get some",
-                        inline: true,
-                    },
+                fields: cards.concat([
                     {
                         name: "\u200b",
                         value: "\u200b",
@@ -40,7 +44,7 @@ export default class MyCards extends Command {
                             "Your deck is empty! Run `addcard <card>` to move a card from your cards to your deck",
                         inline: true,
                     },
-                ],
+                ]),
             });
             message.channel.send({ embed });
         }
