7e57f90e04a189e4ba5d7f0bc89b21a3d7807938
Nigecat <48661288+Nigecat@users.noreply.github.com>
2020-08-19 17:02:10 +1000

    feat: card abilities now show up on cardinfo result

--- src/assets/cards/abilities.ts
+++ src/assets/cards/abilities.ts
@@ -1,7 +1,7 @@
 import { GameState } from "../../commands/commands/Catrd/duel";
 
-export const abilities = {
-    medic: {
+export const abilities: { [name: string]: Ability } = {
+    "Morale Boost": {
         name: "Morale Boost",
         description: "Raises the power of every other card in it's row by 1.",
         execute: async (state: GameState): Promise<void> => {
