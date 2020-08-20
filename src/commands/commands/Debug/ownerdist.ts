98b5455945c19397ef12f3c4e47a1712a2620c62
Nigecat <nigel@nigelq.com>
2020-08-20 12:14:27 +1000

    feat(commant): debug command for checking owner distribution

---
+++ src/commands/commands/Debug/ownerdist.ts
@@ -0,0 +1,27 @@
+import { Message } from "discord.js";
+import { Plexi } from "../../../Plexi";
+import { Command } from "../../Command";
+import { stripIndents } from "common-tags";
+
+export default class OwnerDist extends Command {
+    constructor(client: Plexi) {
+        super(client, {
+            name: "ownerdist",
+            group: "Debug",
+            hidden: true,
+            ownerOwnly: true,
+            description: "Get the bot guild owner distribution",
+        });
+    }
+
+    async run(message: Message): Promise<void> {
+        const uniqueOwners = [...new Set(this.client.guilds.cache.map(({ owner }) => owner.id))];
+        message.channel.send(stripIndents`
+            \`\`\`
+            Total Guilds: ${this.client.guilds.cache.size}
+            Total Unique Guild Owners: ${uniqueOwners.length}
+            Owner distribution: ${(uniqueOwners.length / this.client.guilds.cache.size) * 100}%
+            \`\`\`
+        `);
+    }
+}
