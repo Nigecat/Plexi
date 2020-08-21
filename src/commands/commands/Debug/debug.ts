ff030f595dd87ecc165c427e03bd335645ec5e8e
Nigecat <48661288+Nigecat@users.noreply.github.com>
2020-08-21 17:17:04 +1000

    improve: debug command output data

--- src/commands/commands/Debug/debug.ts
+++ src/commands/commands/Debug/debug.ts
@@ -1,9 +1,12 @@
 import { Plexi } from "../../../Plexi";
+import { getHeapStatistics } from "v8";
 import { Command } from "../../Command";
+import { stripIndents } from "common-tags";
 import { version } from "../../../../package.json";
 import { cpus, arch, totalmem, freemem } from "os";
 import { Message, MessageEmbed } from "discord.js";
-import { stripIndents, oneLine } from "common-tags";
+
+/* eslint-disable prettier/prettier */
 
 export default class Debug extends Command {
     constructor(client: Plexi) {
@@ -17,36 +20,50 @@ export default class Debug extends Command {
     }
 
     run(message: Message): void {
+        const heap = getHeapStatistics();
         const embed = new MessageEmbed({
             color: "RANDOM",
             footer: { text: `Using v${version}` },
+            timestamp: new Date(),
             fields: [
                 {
                     name: "Master Process Information",
                     value: stripIndents`
-                        ${oneLine`
-                            Process is running **${process.release.name} ${process.version}** 
-                            (located at **${process.execPath}**) 
-                            with Process ID **${process.pid}**, 
-                            node is running from **${process.cwd()}**`}
-                        Process has been running for **${process.uptime() / 3600} hours**
+                        Process is running **${process.release.name} ${process.version}** (located at **${process.execPath}**) with Process ID **${process.pid}**, node is running from **${process.cwd()}**.
+                        Process has been running for **${process.uptime() / 3600} hours**.
                     `,
                 },
                 {
                     name: "Operating System Information",
                     value: stripIndents`
-                        ${oneLine`
-                            Hosted on **${process.platform}**, 
-                            using a ${cpus().length}-core processor 
-                            with ${arch()} architecture`}
-                        ${oneLine`
-                            System has a total of **${Math.ceil(totalmem() / 1000000)}MB** RAM, 
-                            of which **${Math.ceil((totalmem() - freemem()) / 1000000)}MB** is in use`}
+                        Hosted on **${process.platform}**, using a ${cpus().length}-core processor with ${arch()} architecture.
+                        System has a total of **${Math.ceil(totalmem() / 1000000)}MB** RAM, of which **${Math.ceil((totalmem() - freemem()) / 1000000)}MB** is in use.
                     `,
                 },
+                {
+                    name: "Memory Information",
+                    value: stripIndents`
+                        Detected **${formatSize(heap.malloced_memory)}** allocated memory with a heap size limit of **${formatSize(heap.heap_size_limit)}**.
+                        Current used heap size is **${formatSize(heap.used_heap_size)}/${formatSize(heap.total_heap_size)}**, physical size is **${formatSize(heap.total_physical_size)}**.
+                    `,
+                }
             ],
         });
 
         message.channel.send({ embed });
     }
 }
+
+function formatSize(size: number): string {
+    const kb = size / 1024;
+    const mb = kb / 1024;
+    const gb = mb / 1024;
+
+    if (kb < 1024) {
+        return `${kb.toFixed(0)}KB`;
+    } else if (kb > 1024 && mb < 1024) {
+        return `${mb.toFixed(0)}MB`;
+    }
+
+    return `${gb.toFixed(0)}GB`;
+};
