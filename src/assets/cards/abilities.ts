0c9a19fb037ec8551e51390ad39e43f96232fbd9
Nigecat <nigel@nigelq.com>
2020-08-23 17:58:51 +1000

    feat: tight bond ability

--- src/assets/cards/abilities.ts
+++ src/assets/cards/abilities.ts
@@ -5,7 +5,7 @@ export const abilities: Record<string, Ability> = {
     "Morale Boost": {
         name: "Morale Boost",
         description: "Raises the power of every other card in it's row by 1.",
-        execute: async ({ turn, card }: { turn: GameUser; card: Card }): Promise<string> => {
+        execute: async ({ turn, card }: AbilityData): Promise<string> => {
             turn.playedCards
                 .filter((c) => c.type === card.type)
                 .forEach((card) => {
@@ -14,17 +14,23 @@ export const abilities: Record<string, Ability> = {
             return `All cards on ${turn.user.username}'s ${card.type} row have gained +1 power!`;
         },
     },
+    "Tight Bond": {
+        name: "Tight Bond",
+        description: "Doubles its power each time you play the same card.",
+        execute: async ({ game, turn }: AbilityData): Promise<string> => {
+            // Find all the tight bond cards they have played
+            const cards = turn.playedCards.filter((card) => card.ability.name === "Tight Bond");
+            // Set each tight bond card's power to be the total number of tight bond cards times the original card power
+            cards.forEach((card) => {
+                card.power = cards.length * game.client.cards.find((c) => c.name === card.name).power;
+            });
+            return `This boosted ${cards.length} cards!`;
+        },
+    },
     /*
     Medic: {
         name: "Medic",
         description: "Brings back one card of your choice that was played during a previous round.",
-        execute: async (game: GameState): Promise<string> => {
-
-        },
-    },
-    "Tight Bond": {
-        name: "Tight Bond",
-        description: "Doubles its power each time you play the same card.",
         execute: async (game: GameState): Promise<string> => {
 
         },
@@ -39,8 +45,14 @@ export const abilities: Record<string, Ability> = {
     */
 };
 
+interface AbilityData {
+    game: GameState;
+    turn: GameUser;
+    card: Card;
+}
+
 export interface Ability {
     name: string;
     description: string;
-    execute: (data: { game: GameState; turn: GameUser; card: Card }) => Promise<string>;
+    execute: (data: AbilityData) => Promise<string>;
 }
