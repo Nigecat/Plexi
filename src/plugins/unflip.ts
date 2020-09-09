d87e042c839635128b541801ecea117b39abffaa
Nigecat <nigel@nigelq.com>
2020-09-09 11:37:47 +1000

    Create plugin

---
+++ src/plugins/unflip.ts
@@ -0,0 +1,14 @@
+import { Plexi } from "../Plexi";
+import { Message } from "discord.js";
+
+// If someone does /tableflip respond with /unflip
+export default function (client: Plexi): void {
+    client.on("message", async (message: Message) => {
+        // Ignore bot messages
+        if (message.author.bot) return;
+
+        if (message.content.endsWith("(╯°□°）╯︵ ┻━┻")) {
+            message.channel.send("unflip");
+        }
+    });
+}
