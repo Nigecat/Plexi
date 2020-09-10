7cd958dac2912079dda0575bae9fc8617b6a44cb
Nigecat <nigel@nigelq.com>
2020-09-10 19:12:33 +1000

    chore: setup api framework (rocket)

--- api/src/main.rs
+++ api/src/main.rs
@@ -1,3 +1,25 @@
+#![feature(proc_macro_hygiene, decl_macro)]
+
+#[macro_use] extern crate rocket;
+
+use std::io::Cursor;
+use rocket::response::{Response, Body};
+
+/// Manipulate an image from a url with the given parameters, returns a buffer
+#[get("/manipulate/<url>?<posterize>&<contrast>&<pixelate>")]
+fn manipulate<'a>(url: String, posterize: f64, contrast: Option<f32>, pixelate: Option<f32>) -> Response<'a> {
+    let img_raw = reqwest::blocking::get(&url).unwrap().bytes().unwrap();
+    let image = image::load_from_memory(&img_raw).unwrap();
+
+    // Apply our arguments
+   // image.adjust_contrast(contrast.unwrap_or_else(|| 0.75));
+
+    let mut response = Response::new();
+    let body = Body::Chunked(Cursor::new(img_raw), 4);
+    response.set_raw_body(body);
+    return response;   
+}
+
 fn main() {
-    println!("Hello, world!");
+    rocket::ignite().mount("/plexi/api/", routes![manipulate]).launch();
 }
