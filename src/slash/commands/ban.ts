7adba1e7a795d42e5aacf8c2078291c1b3a772dd
Nigecat <nigel@nigelq.com>
2020-12-16 18:29:41 +1100

    Add kick and ban slash commands

---
+++ src/slash/commands/ban.ts
@@ -0,0 +1,25 @@
+import { ephemeral, user } from "../utils";
+import { Plexi } from "../../Plexi";
+import { InteractionData, SlashCommand, SlashCommandResponse } from "../SlashCommand";
+
+export default class Ban extends SlashCommand {
+    constructor(client: Plexi) {
+        super(client, {
+            name: "ban",
+            description: "Ban a user",
+            options: [user("The user to ban")],
+        });
+    }
+
+    async handler(interaction: InteractionData): Promise<SlashCommandResponse> {
+        const guild = await this.client.guilds.fetch(interaction.guild_id);
+        const user = await guild.members.fetch(interaction.data.options[0].value);
+
+        if (user.bannable) {
+            await user.ban();
+            return ephemeral(`${user} successfully banned.`);
+        } else {
+            return ephemeral(`Unable to ban ${user}, I can't ban anyone with a higher role than me.`);
+        }
+    }
+}
---
+++ src/slash/commands/kick.ts
@@ -0,0 +1,25 @@
+import { ephemeral, user } from "../utils";
+import { Plexi } from "../../Plexi";
+import { InteractionData, SlashCommand, SlashCommandResponse } from "../SlashCommand";
+
+export default class Kick extends SlashCommand {
+    constructor(client: Plexi) {
+        super(client, {
+            name: "kick",
+            description: "Kick a user",
+            options: [user("The user to kick")],
+        });
+    }
+
+    async handler(interaction: InteractionData): Promise<SlashCommandResponse> {
+        const guild = await this.client.guilds.fetch(interaction.guild_id);
+        const user = await guild.members.fetch(interaction.data.options[0].value);
+
+        if (user.kickable) {
+            await user.kick();
+            return ephemeral(`${user} successfully kicked.`);
+        } else {
+            return ephemeral(`Unable to kick ${user}, I can't kick anyone with a higher role than me.`);
+        }
+    }
+}
