9c6a4c967e730e5b34a313083c898a7e1eb7bd59
Nigecat <nigel@nigelq.com>
2020-12-17 09:01:25 +1100

    Improve invalid slash command handling

--- src/events/raw/INTERACTION_CREATE.ts
+++ src/events/raw/INTERACTION_CREATE.ts
@@ -1,16 +1,25 @@
 import axios from "axios";
-import { DISCORD_API } from "../../constants";
 import { Plexi } from "../../Plexi";
-import { InteractionData } from "../../slash/SlashCommand";
+import { ephemeral } from "../../slash/utils";
+import { DISCORD_API } from "../../constants";
+import { InteractionData, SlashCommandResponse } from "../../slash/SlashCommand";
 
 export default async function (client: Plexi, data: InteractionData): Promise<void> {
     // Get the name of the slash command that was run
-    const name = data.data.name;
+    const name = data.data.name.toLowerCase();
 
     client.emit("debug", `Running slash command ${name}`);
 
-    // Look it up and run the handler
-    const response = await client.slashCommands.get(name.toLowerCase()).handler(data, data.data.options);
+    let response: void | SlashCommandResponse;
+
+    // If we can't find this command show an error
+    if (!client.slashCommands.has(name)) {
+        client.emit("error", new Error(`User attempted to run unkwnown slash command: ${name}`));
+        response = ephemeral("I could not find a command with that name, it might no longer exist.");
+    } else {
+        // Otherwise look it up and run the handler
+        response = await client.slashCommands.get(name).handler(data, data.data.options);
+    }
 
     // If the handler returned any data run the callback with it
     if (response) {
