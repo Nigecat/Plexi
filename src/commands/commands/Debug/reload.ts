e8650f2dabea4ba94a126e0076582412f2c7504a
Nigecat <nigel@nigelq.com>
2020-12-16 16:20:44 +1100

    Add support for reloading slash commands

--- src/commands/commands/Debug/reload.ts
+++ src/commands/commands/Debug/reload.ts
@@ -3,6 +3,7 @@ import loadCommands from "../../";
 import { Message } from "discord.js";
 import { Plexi } from "../../../Plexi";
 import { Command } from "../../Command";
+import loadSlashCommands from "../../../slash";
 
 export default class Reload extends Command {
     constructor(client: Plexi) {
@@ -33,6 +34,26 @@ export default class Reload extends Command {
         } catch (err) {
             this.client.emit("error", err);
             message.channel.send(`Unable to reload commands: \`${err}\``);
+        }
+
+        // Do the same thing but for slash commands
+        try {
+            // Loop through each command and remove it from the module cache
+            this.client.slashCommands.forEach(
+                (command) =>
+                    delete require.cache[
+                        require.resolve(resolve(__dirname, "..", "..", "..", "slash", "commands", `${command.name}.js`))
+                    ],
+            );
+
+            // Now that they are all unloaded we can load them again normally
+            this.client.slashCommands = await loadSlashCommands(this.client);
+
+            message.channel.send(`Successfully reloaded ${this.client.slashCommands.size} slash commands!`);
+            this.client.emit("debug", `Reloaded ${this.client.slashCommands.size} slash commands`);
+        } catch (err) {
+            this.client.emit("error", err);
+            message.channel.send(`Unable to reload slash commands: \`${err}\``);
         }
     }
 }
