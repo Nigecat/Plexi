8a9de21db7400585547531f59f7486345a80904a
Nigecat <48661288+Nigecat@users.noreply.github.com>
2020-12-17 14:46:24 +1100

    Add slash command length check

--- src/slash/index.ts
+++ src/slash/index.ts
@@ -9,7 +9,14 @@ export default async function loadSlashCommands(client: Plexi): Promise<Collecti
     const commands: Collection<string, SlashCommand> = new Collection();
 
     // Read all the files in the commands/ directory, we ignore any folders
-    const files = await getFiles(resolve(__dirname, "commands"));
+    let files = await getFiles(resolve(__dirname, "commands"));
+
+    // Check to make sure we don't go over the 50 slash command limit
+    if (files.length > 50) {
+        client.emit("error", new Error("Over 50 slash commands detected, remove excess commands."));
+        files = files.slice(0, 50);
+    }
+
     for (let i = 0; i < files.length; i++) {
         const command: SlashCommand = new (await import(files[i].path)).default(client);
         commands.set(command.name.toLowerCase(), command);
