1bd3d79307cfd0cdbde35106dc0e6b0a213e7160
Nigecat <48661288+Nigecat@users.noreply.github.com>
2020-12-18 16:22:04 +1100

    Improve html styling

--- debug/mix.exs
+++ debug/mix.exs
@@ -20,7 +20,7 @@ defmodule Debug.MixProject do
     def application do
         [
             mod: {Debug.Application, []},
-            extra_applications: [:logger, :runtime_tools]
+            extra_applications: [:logger, :runtime_tools, :os_mon]
         ]
     end
 
