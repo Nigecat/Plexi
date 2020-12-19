b4d88bb8cc2e9f49c511ae7480b9f1b0f90b1339
Nigecat <48661288+Nigecat@users.noreply.github.com>
2020-12-19 22:00:06 +1100

    revert: make interaction callback use manual post request
    We were getting 404 errors from the internal client due to unknown reasons.

--- src/events/raw/INTERACTION_CREATE.ts
+++ src/events/raw/INTERACTION_CREATE.ts
@@ -1,5 +1,7 @@
+import axios from "axios";
 import { Plexi } from "../../Plexi";
 import { ephemeral } from "../../slash/utils";
+import { DISCORD_API } from "../../constants";
 import { InteractionData, SlashCommandResponse } from "../../slash/SlashCommand";
 
 export default async function (client: Plexi, data: InteractionData): Promise<void> {
@@ -21,7 +23,9 @@ export default async function (client: Plexi, data: InteractionData): Promise<vo
 
     // If the handler returned any data run the callback with it
     if (response) {
-        client.discord.interactions(data.id, data.token).callback.post({
+        await axios({
+            method: "post",
+            url: `${DISCORD_API}/interactions/${data.id}/${data.token}/callback`,
             data: response,
         });
     }
