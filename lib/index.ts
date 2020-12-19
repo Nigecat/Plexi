460deec92af259e4097e54e1edbff6fd9d31fd33
Nigecat <nigel@nigelq.com>
2020-12-19 18:01:04 +1100

    Fix improper timing

--- lib/index.ts
+++ lib/index.ts
@@ -1,3 +1,7 @@
+/* eslint-disable */
+
+import { manipulateImage } from "../src/utils/image";
+
 export interface lib {
     manipulateImage: (input: string, output: string, contrast: number) => void;
 }
@@ -5,9 +9,12 @@ export interface lib {
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 const lib: lib = require("./build/Release/lib");
 
-const start = process.hrtime();
-lib.manipulateImage("W:/Meme-Formats/stonks.png", "stonks.jpg", 300);
-const end = process.hrtime();
-console.log(`${end[0] - start[0]}.${end[1] - start[1]}`);
+(async () => {
+    const start = process.hrtime.bigint();
+    lib.manipulateImage("W:/Meme-Formats/stonks.png", "stonks.jpg", 300);
+    //await manipulateImage("https://cdn.discordapp.com/attachments/587782757824987141/789710109755572224/unknown.png", 1, 0.3);
+    const end = process.hrtime.bigint();
+    console.log(`\n\nProcess took ${(end - start) / 1_000_000n}ms`);
+})();
 
 export default lib;
